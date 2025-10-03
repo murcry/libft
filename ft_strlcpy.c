e7c26f7bdc1450eaa8b6b24b5ca0c34c38d9e1de
murcry <diego.brasero@gmail.com>
2025-10-03 16:44:44 +0200

    ft_strlcpy at half

---
+++ ft_strlcpy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: digonza2 <digonza2@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/03 15:40:31 by digonza2          #+#    #+#             */
+/*   Updated: 2025/10/03 15:40:31 by digonza2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	
+	i = 0;
+	while(i < size)
+	{
+	}
+	
+	return (ft_strlen(src));
+}
