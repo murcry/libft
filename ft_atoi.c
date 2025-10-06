9b1ccc5657c07019aaa22acec6459fd3db6f3a36
murcry <diego.brasero@gmail.com>
2025-10-06 13:09:03 +0200

    ft_atoi

---
+++ ft_atoi.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: digonza2 <digonza2@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/03 16:46:30 by digonza2          #+#    #+#             */
+/*   Updated: 2025/10/03 16:46:30 by digonza2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_atoi(const char *nptr)
+{
+    int i;
+    int num;
+    int sgn;
+
+    i = 0;
+    sgn = 1;
+    num = 0;
+    if(nptr[0] == '-')
+    {
+        sgn *= -1;
+        i++;
+    }
+    while (nptr[i] >= '0' && nptr[i] <= '9')
+    {
+        num = num + nptr[i] - '0';
+        num *= 10;
+        i++;
+    }
+    return ((num/10) * sgn);
+}
+int main(void)
+{
+    int     num;
+    char    number[] = "-146";
+
+    num = ft_atoi(number);
+    printf("%d", num);
+    return (0);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: digonza2 <digonza2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 12:45:44 by digonza2          #+#    #+#             */
-/*   Updated: 2025/10/03 16:37:23 by digonza2         ###   ########.fr       */
+/*   Updated: 2025/10/06 12:51:47 by digonza2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,5 +38,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 
 size_t  ft_strlcpy(char *dest, const char *src, size_t size);
+
+int ft_atoi(const char *nptr);
 
 #endif
