35076c302168ee3727cce6e57a4108b034e08d35
Diego Gonzalez <digonza2@student.42madrid.com>
2025-10-09 13:11:05 +0200

    done in 42 madrid

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,15 +6,32 @@
 /*   By: digonza2 <digonza2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 15:40:31 by digonza2          #+#    #+#             */
-/*   Updated: 2025/10/03 15:40:31 by digonza2         ###   ########.fr       */
+/*   Updated: 2025/10/09 12:53:29 by digonza2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //TODO
+/* The strlcpy() and strlcat() functions copy and concatenate strings re‐
+    spectively.  They are designed to be safer, more consistent, and less er‐
+    ror prone replacements for strncpy(3) and strncat(3).  Unlike those func‐
+    tions, strlcpy() and strlcat() take the full size of the buffer (not just
+    the length) and guarantee to NUL-terminate the result (as long as size is
+    larger than 0 or, in the case of strlcat(), as long as there is at least
+    one byte free in dst).  Note that a byte for the NUL should be included
+    in size.  Also note that strlcpy() and strlcat() only operate on true “C”
+    strings.  This means that for strlcpy() src must be NUL-terminated and
+    for strlcat() both src and dst must be NUL-terminated.
 
+    The strlcpy() function copies up to size - 1 characters from the NUL-ter‐
+    minated string src to dst, NUL-terminating the result.
+
+    The strlcat() function appends the NUL-terminated string src to the end
+    of dst.  It will append at most size - strlen(dst) - 1 bytes, NUL-termi‐
+    nating the result.
+*/
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 	
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: digonza2 <digonza2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 12:45:44 by digonza2          #+#    #+#             */
-/*   Updated: 2025/10/07 17:04:00 by digonza2         ###   ########.fr       */
+/*   Updated: 2025/10/09 13:10:42 by digonza2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,10 +37,26 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 
 void	*ft_memmove(void *dest, const void *src, size_t n);
 
-size_t  ft_strlcpy(char *dest, const char *src, size_t size);
+size_t  ft_strlcpy(char *dest, const char *src, size_t size);	//TODO
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size);	//TODO
+
+int		ft_toupper(int c);										//TODO
+
+int		ft_tolower(int c);										//TODO
+
+/*he strchr() function returns a pointer to the first occurrence of the 
+	character c in the string s.*/
+
+char 	*ft_strchr(const char *s, int c);						//TODO
+
+/*The strrchr() function returns a pointer to the last occurrence of the
+	character c in the string s.*/
+
+char	*ft_strrchr(const char *s, int c);						//TODO
 
 int     ft_atoi(const char *nptr);
 
-char    *ft_itoa(int n);
+char    *ft_itoa(int n);										//TODO
 
 #endif
