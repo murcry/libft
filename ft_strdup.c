796eedcd5333acb02bc2d82fe3ca9ecd0bddb0bc
murcry <diego.brasero@gmail.com>
2025-10-17 15:13:42 +0200

    All first part with PACO STRICT

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: digonza2 <digonza2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 13:00:30 by digonza2          #+#    #+#             */
-/*   Updated: 2025/10/17 13:05:22 by digonza2         ###   ########.fr       */
+/*   Updated: 2025/10/17 15:08:05 by digonza2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,20 @@
 
 char	*ft_strdup(const char *s)
 {
+	char	*copy;
+	size_t	size_s;
+	int		i;
+
+	i = 0;
+	size_s = ft_strlen(s);
+	copy = malloc(size_s + 1);
+	if (copy == NULL)
+		return (NULL);
+	while (i < (int)(size_s))
+	{
+		copy[i] = s[i];
+		i++;
+	}
+	copy[i] = '\0';
+	return (copy);
 }
