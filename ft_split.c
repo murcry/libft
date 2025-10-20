80326ca4f8dacce17123efef5b8654dd9471606d
murcry <diego.brasero@gmail.com>
2025-10-21 01:49:52 +0200

    split main

--- ft_split.c
+++ ft_split.c
@@ -6,22 +6,61 @@
 /*   By: digonza2 <digonza2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 13:15:02 by digonza2          #+#    #+#             */
-/*   Updated: 2025/10/20 13:56:16 by digonza2         ###   ########.fr       */
+/*   Updated: 2025/10/21 01:49:25 by digonza2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 static unsigned int	count_elements(char const *s, char c)
 {
-	int				i;
-	unsigned int	count;
+	size_t	i;
+	int		count;
 
+	if (s == NULL)
+		return (0);
 	i = 0;
 	count = 0;
 	while (s[i])
 	{
+		while (s[i] == c)
+			i++;
+		if (s[i] != c && s[i] != '\0')
+			count++;
+		while (s[i] != c && s[i] != '\0')
+			i++;
 	}
+	return (count);
+}
+
+static char	*ft_scissors(size_t start, size_t end, const char *s)
+{
+	size_t	s_len;
+	size_t	i;
+	char	*word;
+
+	if (start > end)
+		return (NULL);
+	s_len = end - start + 1;
+	word = malloc(s_len + 1);
+	if (word == NULL)
+		return (NULL);
+	i = 0;
+	while (i < s_len)
+	{
+		word[i] = s[start + i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+static void	ft_free_all(char **list, size_t elements_num)
+{
+	while (elements_num > 0)
+		free(list[--elements_num]);
+	free(list);
 }
 
 /*
@@ -36,4 +75,16 @@ static unsigned int	count_elements(char const *s, char c)
 
 char	**ft_split(char const *s, char c)
 {
+	size_t	elements_num;
+	char	**list;
 }
+
+// int	main(void)
+// {
+// 	char			s[30] = "       Hola Mundo   Bonito    ";
+// 	unsigned int	word;
+
+// 	word = count_elements(s, ' ');
+// 	printf("%d\n", word);
+// 	return (0);
+// }
