5445cd1a00c53dd6fbe16d4882b3cd583ac9ebbf
murcry <diego.brasero@gmail.com>
2025-10-21 19:36:16 +0200

    libft with no bonus completed :D

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: digonza2 <digonza2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 17:36:34 by digonza2          #+#    #+#             */
-/*   Updated: 2025/10/21 18:06:04 by digonza2         ###   ########.fr       */
+/*   Updated: 2025/10/21 19:33:36 by digonza2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,17 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n == INT_MI)
+	if (n == INT_MIN)
+		write(fd, "-2147483648", 11);
+	else
+	{
+		if (n < 0)
+		{
+			write(fd, "-", 1);
+			n *= -1;
+		}
+		if (n >= 10)
+			ft_putnbr_fd(n / 10, fd);
+		ft_putchar_fd((n % 10) + '0', fd);
+	}
 }
