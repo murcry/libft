6ad3fe6f880435175856e52cbb437f223f078c8e
murcry <diego.brasero@gmail.com>
2025-10-21 16:19:29 +0200

    problemsxd

---
+++ ft_striteri.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: digonza2 <digonza2@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/21 15:44:53 by digonza2          #+#    #+#             */
+/*   Updated: 2025/10/21 15:53:10 by digonza2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+ * This function iterates through the string 's', passing the index of each
+ * character and a pointer to the character itself to the function 'f'.
+ * This allows for in-place modification of the string.
+ * s: The string to iterate over.
+ * f: The function to apply to each character.
+ * Returns nothing.
+ */
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: digonza2 <digonza2@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 12:45:44 by digonza2          #+#    #+#             */
-/*   Updated: 2025/10/21 15:01:42 by digonza2         ###   ########.fr       */
+/*   Updated: 2025/10/21 15:50:11 by digonza2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -309,5 +309,16 @@ char	*ft_itoa(int n);
  */
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+
+/*
+ * This function iterates through the string 's', passing the index of each
+ * character and a pointer to the character itself to the function 'f'.
+ * This allows for in-place modification of the string.
+ * s: The string to iterate over.
+ * f: The function to apply to each character.
+ * Returns nothing.
+ */
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 
 #endif
